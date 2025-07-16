95a5bf8858f2e9124fa870c31cfa677b5770fe21
tosik-14 <anton.mikhailouski@gmail.com>
2025-07-16 13:06:51 +0300

    feat: user can scroll between weeks

--- src/features/SchedulePage/ui/ScheduleWeek/ScheduleWeek.style.ts
+++ src/features/SchedulePage/ui/ScheduleWeek/ScheduleWeek.style.ts
@@ -4,4 +4,19 @@ export const styles = StyleSheet.create({
     weekBlock: {
         marginBottom: 100,
     },
+
+    dayBlock: {
+        marginHorizontal: 8,
+        marginTop: 30,
+    },
+    dayBlockTitle: {
+        flex: 1,
+        marginHorizontal: 13,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    weekNumber: {
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
+    }
 });
--- src/features/SchedulePage/ui/ScheduleWeek/ScheduleWeek.tsx
+++ src/features/SchedulePage/ui/ScheduleWeek/ScheduleWeek.tsx
@@ -1,27 +1,68 @@
-import {View, Text} from "react-native";
-import {Schedule} from "@/src/entities/schedule/types";
-import {globalStyles} from "@/src/shared/styles/globalStyles";
-import {useThemeColors} from "@/src/shared/hooks/useThemeColor";
-import ScheduleDay from "@/src/features/SchedulePage/ui/ScheduleDay/ScheduleDay";
-import {styles} from '@/src/features/SchedulePage/ui/ScheduleWeek/ScheduleWeek.style'
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, ScrollView, NativeSyntheticEvent, NativeScrollEvent } from 'react-native';
+import { Week } from '@/src/entities/schedule/week/types';
+import { Schedule } from '@/src/entities/schedule/types';
+import ScheduleLessons from '@/src/features/SchedulePage/ui/ScheduleLessons/ScheduleLessons';
+import getVirtualWeek from '@/src/features/SchedulePage/lib/getVirtualWeek';
+import { globalStyles } from '@/src/shared/styles/globalStyles';
+import { useThemeColors } from '@/src/shared/hooks/useThemeColor';
+import { styles } from './ScheduleWeek.style';
 
 interface ScheduleProps {
-    schedule: Schedule,
+    schedule: Schedule;
 }
 
-export default function ScheduleWeek({schedule} : ScheduleProps) {
-    const { backgroundColor, textColor, upBackgroundColor } = useThemeColors();
+export default function ScheduleWeek({ schedule }: ScheduleProps) {
+    const [weekOffset, setWeekOffset] = useState(0);
+    const [selectedWeek, setSelectedWeek] = useState<Week | null>(null);
+    const scrollViewRef = useRef<ScrollView | null>(null);
+    const { textColor } = useThemeColors();
+
+    useEffect(() => {
+        const virtualWeek = getVirtualWeek(weekOffset, schedule);
+        setSelectedWeek(virtualWeek);
+    }, [weekOffset, schedule]);
+
+    const handleScrollEnd = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+        const { contentOffset, contentSize, layoutMeasurement } = event.nativeEvent;
+        const offsetY = contentOffset.y;
+        const maxOffsetY = contentSize.height - layoutMeasurement.height;
+
+        if (offsetY <= 0) {
+            setWeekOffset(prev => prev - 1);
+        } else if (offsetY >= maxOffsetY) {
+            setWeekOffset(prev => prev + 1);
+        }
+    };
+
+    if (!selectedWeek) return null;
 
     return (
-        <View>
-            {schedule.week.map((week) => (
-                <View key={week.id} style={styles.weekBlock}>
-                    
-                    <ScheduleDay week={week} />
-
+        <ScrollView
+            ref={scrollViewRef}
+            onScrollEndDrag={handleScrollEnd}
+            scrollEventThrottle={16}
+        >
+            {selectedWeek.weekDays.map((day) => (
+                <View key={day.id} style={styles.dayBlock}>
+                    <View style={styles.dayBlockTitle}>
+                        <View>
+                            <Text style={[globalStyles.fontSB16, { color: textColor }]}>
+                                {day.dayName}
+                            </Text>
+                            <Text style={[globalStyles.fontSB16, { color: textColor }]}>
+                                {day.date}
+                            </Text>
+                        </View>
+                        <View style={styles.weekNumber}>
+                            <Text style={[globalStyles.fontSB14, { color: textColor }]}>
+                                {'нед. '}{selectedWeek?.weekAlternation}
+                            </Text>
+                        </View>
+                    </View>
+                    <ScheduleLessons weekDays={day} />
                 </View>
             ))}
-        </View>
-
+        </ScrollView>
     );
 }
